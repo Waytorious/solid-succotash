f7fb5918097f19647c6eaa641c6f97b1aa3d245a
Waytorious <waytoriousgamer@gmail.com>
2026-09-18 16:54:05 +0300

    Wrote smth in second.cpp

---
+++ second.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <thread>
+
+using namespace std;
+
+std::atomic<int> global{0};
+
+int main(){
+    std::thread a{b};
+    std::thread c{b};
+
+    std::cout << global << std::endl;
+
+    a.join();
+    c.join();
+
+    std::cout << global << std::endl;
+
+
+    return 0;
+}
+
+void b(){
+    for (int i=0; i<1000000; i++){
+        global++;
+    }
+    
+}
